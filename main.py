b41cbac15b5db5e861fa5c08f305862c0d1a1835
Marián Svitek <marian.svitek99@gmail.com>
2021-02-06 23:24:01 +0100

    before adding NotesTable class

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import mysql.connector
 from note import Note
 from datetime import date
-from typing import List, Tuple
+from typing import List, Tuple, Optional
+
+
+class NotesTable:
+    pass
 
 
 db_connection = mysql.connector.connect(host="sql7.freesqldatabase.com",
@@ -77,18 +81,80 @@ def select_whole_table(table: str = "Notes") \
     return c.fetchall()
 
 
-# nakupne_polozky = "- milk\n- eggs\n- salt"
-# note1 = Note("Shopping", "Marek Nagy", date(2021, 1, 5), nakupne_polozky)
+def select_note_by_id(note_id: int, table: str = "Notes") -> Optional[Note]:
+    try:
+        c.execute("SELECT * FROM {} WHERE noteID = {}".format(table, note_id))
+        got: Tuple[str, str, date, str] = c.fetchone()
+        if got is None:
+            print("Table '{}' doesnt contain a record with id {}".
+                  format(table, note_id))
+            return
+        return Note(got[0], got[1], got[2], got[3])
+    except mysql.connector.errors.ProgrammingError:
+        print("Selection failed due to unknown error!")
+        return
+
+
+def draw_whole_table(table: str = "Notes"):
+    records_lst: List[Tuple[str, str, date, str]] = select_whole_table(table)
+    print("-" * 76)
+    print("| {:6} | {:^20} | {:^20} | {:^10} | {:256}"
+          .format("noteID", "title", "author", "published", "content"))
+    print("-" * 76)
+    for record in records_lst:
+        print("| ", end="")
+        print("{:^6} ".format(record[0]), end="")
+        print("| ", end="")
+        print("{:^20} ".format(record[1]), end="")
+        print("| ", end="")
+        print("{:^20} ".format(record[2]), end="")
+        print("| ", end="")
+        print("{:^10} ".format(str(record[3])), end="")
+        print("| ", end="")
+        print("{:256}".format(record[4].replace("\n", "\n" + " "*70)))
+        print("-" * 76)
+
+
+def change_note_by_id(note_id: int, new_title: str = None,
+                      new_author: str = None, new_date: date = None,
+                      new_content: str = None):
+    if new_title is not None and len(new_title) > 20:
+        print("Lenght of new_title exceeded the maximum varchar!")
+    if new_author is not None and len(new_author) > 20:
+        print("Length of new_author exceeded the maximum varchar!")
+    if new_content is not None and len(new_content) > 256:
+        print("Length of new_content exceeded the maximum varchar!")
+
+    try:
+        c.execute("SELECT * FROM Notes WHERE noteID = {}".format(note_id))
+        record: Tuple[str, str, date, str] = c.fetchone()
+        new_title = new_title if new_title is not None else record[1]
+        new_author = new_author if new_author is not None else record[2]
+        new_date = new_date if new_date is not None else record[3]
+        new_date = str(new_date)
+        new_content = new_content if new_content is not None else record[4]
+        c.execute("UPDATE Notes SET "
+                  "title = '{}',"
+                  "author = '{}',"
+                  "published_date = '{}',"
+                  "content = '{}'"
+                  "WHERE noteID = {}"
+                  .format(new_title, new_author, new_date, new_content,
+                          note_id))
+        print("Update was succesfull!")
+    except mysql.connector.errors.ProgrammingError:
+        print("Update has failed!")
+        return
+
+
+nakupne_polozky = "- milk\n- eggs\n- salt"
+note1 = Note("Shopping", "Marek Nagy", date(2021, 1, 5), nakupne_polozky)
 # insert_note(note1)
 # print(select_whole_table())
+# draw_whole_table()
 
-
-
-
-
-
-
-
+# change_note_title_by_id(28, "Shopping")
+change_note_by_id(28, new_title="Shopping45", new_author="Random", new_date=date(1999, 1, 1), new_content="Hello World!")
 
 
 
