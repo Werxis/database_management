7cdf9034d0ca74e91e0edd7837d578613e49af20
Marek Nagy <62247071@cuni.cz>
2021-03-20 16:07:12 +0100

    [WIP] Adding docstrings Refined returned status codes

--- notes_rest.py
+++ notes_rest.py
@@ -31,6 +31,10 @@ register_error_message = {
     'message': "Username is already being used."
     }
 
+note_not_found_error = {
+    'message': "Note not found."
+    }, 400
+
 @app.before_first_request
 def create_table():
     db.create_all()
@@ -39,15 +43,19 @@ class LoginView(Resource):
     # user for testing login
     #@login_required
     def get(self):
-        if current_user.is_authenticated:
-            return {'message': 'You are logged in.'}
-        else:
-            return {'message': 'You are not logged in.'}
+        '''
+            Used for testing if current user is logged in.
+        '''
+        return {'logged_in': current_user.is_authenticated}
 
     # used for login
-    def put(self):
+    def post(self):
+        '''
+            Login existing user.
+            JSON is expected to have "username" and "password" attributes.
+        '''
         if current_user.is_authenticated:
-            return {'message':'User already logged in.'}, 403
+            return {'message':'User already logged in.'}
 
         input_json = json.loads(request.get_data().decode('UTF-8'))
         username = input_json['username']
@@ -60,12 +68,17 @@ class LoginView(Resource):
             return {
                 'success': True,
                 'id': user.id
-                }
+                }, 200
         else:
             return login_error_message
-    
+
+class RegisterView(Resource):
     # used for register
     def post(self):
+        '''
+            Register new user.
+            JSON is expected to have "username" and "password" attributes.
+        '''
         input_json = json.loads(request.get_data().decode('UTF-8'))
         print("Received JSON: {}".format(input_json))
         username = input_json['username']
@@ -97,47 +110,67 @@ class LogoutView(Resource):
             'message': 'You are now logged out.'
         }
 
-class NoteView(Resource):
-    @login_required
-    def get(self):
-        pass
-
-    @login_required
-    def put(self):
-        user = current_user
-        input_json = json.loads(request.get_data().decode('UTF-8'))
-        title = input_json["title"]
-        body = input_json["body"]
-
-        note = NoteModel.query.filter_by(id=id, ).first()
-
-        if note is None:
-            note = NoteModel(title, body)
-        else:
-            note.update(body)
-        
-        db.session.add(note)
-        db.session.commit()
-
-        return note.json()
-    
+class NoteView(Resource):    
     @login_required
     def delete(self):
         input_json = json.loads(request.get_data().decode('UTF-8'))
-        title = input_json["title"]
+        id = input_json["id"]
 
-        note = NoteModel.query.filter_by(title=title).first()
+        note = NoteModel.query.filter_by(id=id).first()
         if note is not None:
             db.session.delete(note)
             db.session.commit()
             return {'message': "Note deleted."}
         else:
-            return {'message': "Note not found."}, 404
+            return note_not_found_error, 400
 
-api.add_resource(NoteView, '/notes')
+    @login_required
+    def post(self):
+        input_json = json.loads(request.get_data().decode('UTF-8'))
+        title = input_json["title"]
+        body = input_json["body"]
+
+        note = NoteModel(current_user, title, body)
+        
+        db.session.add(note)
+        db.session.commit()
+
+        return note.json()
+
+class NoteUpdateView(Resource):
+    @login_required
+    def post(self):
+        input_json = json.loads(request.get_data().decode('UTF-8'))
+        id = input_json['id']
+        title = input_json["title"]
+        body = input_json["body"]
+
+        note = NoteModel.query.filter_by(id=id).first()
+        if note is not None:
+            note.update(title, body)
+            db.session.commit()
+            return note.json(), 200
+        else:
+            return note_not_found_error
+
+class NotesView(Resource):
+    @login_required
+    def get(self):
+        '''
+            Get all notes of currently logged in user.
+        '''
+        notes = NoteModel.query.filter_by(user=current_user).all()
+        return {
+            'notes': list(map(lambda x: x.json(), notes))
+        }
 
 api.add_resource(LoginView, '/login')
 api.add_resource(LogoutView, '/logout')
+api.add_resource(RegisterView, '/register')
+
+api.add_resource(NoteView, '/note')
+api.add_resource(NotesView, '/notes')
+api.add_resource(NoteUpdateView, '/note/update')
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(64)
